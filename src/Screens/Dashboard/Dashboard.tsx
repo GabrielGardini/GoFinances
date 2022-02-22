ffe4e57187a3c5ce322a73d54d212b4e3b1fca9d
Gabriel Gardini <gabriel.gardini@bhut.com.br>
2022-02-22 13:35:25 -0300

    primeiro commit

---
+++ src/Screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Container, Title} from './DashboardStyle';
+
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Title >Dashboard</Title>
+        </Container>
+    )
+}
